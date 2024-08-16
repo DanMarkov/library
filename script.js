7b2d5269f3fc73245c0fe409784d0c719084c305
Kenny <89842110+DanMarkov@users.noreply.github.com>
2024-08-16 15:20:38 +0800

    feat: add status if the book was read

--- script.js
+++ script.js
@@ -3,28 +3,28 @@ const myLibrary = [
     title: "The Long Run: A Creative Inquiry",
     author: "Stacey D'Erasmo",
     pages: 192,
-    read: false
+    read: "No"
   },
 
   {
     title: "Fifteen Cents on the Dollar: How Americans Made the Black-White Wealth Gap ",
     author: "Louise Story",
     pages: 464,
-    read: false
+    read: "No"
   },
 
   {
     title: "Circle of Hope: A Reckoning with Love, Power, and Justice in an American Church",
     author: "Eliza Griswold",
     pages: 352,
-    read: false
+    read: "No"
   },
 
   {
     title: "Devil's Contract: The History of the Faustian Bargain",
     author: " Ed Simon",
     pages: 356,
-    read: false
+    read: "No"
   }
 ];
 
@@ -35,7 +35,7 @@ function bookBox(title, author, pages, isRead) {
   const book_container = document.createElement("div");
   book_container.classList.add("book_container");
   
-  container.appendChild(book_container);
+  container.prepend(book_container);
   
   const h3 = document.createElement("h3");
   h3.textContent = title;
@@ -48,19 +48,35 @@ function bookBox(title, author, pages, isRead) {
   const hline = document.createElement("div");
   hline.classList.add("horizontal_line");
   book_container.appendChild(hline);
-  
+
+  const pagesContainer = document.createElement("div");
+  pagesContainer.classList.add("pages_container");
+  book_container.appendChild(pagesContainer);
+
   const p = document.createElement("p");
-  p.textContent = pages;
-  book_container.appendChild(p);
+  p.textContent = "Pages:";
+  pagesContainer.appendChild(p);
   
+  const span = document.createElement("span");
+  span.textContent = pages;
+  pagesContainer.appendChild(span);
+
+  const statusBlock = document.createElement("div");
+  statusBlock.classList.add("status_block");
+  book_container.appendChild(statusBlock);
+
+  const statusQuestion = document.createElement("p");
+  statusQuestion.textContent = "Did you read this book?";
+  statusBlock.appendChild(statusQuestion);
+
   const read = document.createElement("p");
   read.classList.add("status");
   read.textContent = isRead;
-  book_container.appendChild(read);
+  statusBlock.appendChild(read);
 
   const deleteButton = document.createElement("button");
-  deleteButton.textContent = "Delete this book!";
-  deleteButton.classList.add("delete-button");
+  deleteButton.textContent = "Delete";
+  deleteButton.classList.add("delete_button");
   book_container.appendChild(deleteButton);
 
 }
@@ -87,7 +103,7 @@ function addBookToLibrary() {
 
 function wholeBookLibrary(library) {
   for (let i = 0; i < library.length; i++) {
-    console.log(library[i]);
+    // console.log(library[i]);
     bookBox(library[i].title, library[i].author, library[i].pages, library[i].read);
   }
 }
@@ -125,13 +141,24 @@ function changeStatus() {
   const statuses = document.querySelectorAll(".status");
 
   statuses.forEach((status) => {
+    // if (status.textContent === "Yes") {
+    //   status.classList.add("yes_sign");
+    // } else {
+    //   status.classList.remove("yes_sign");
+    // }
+
     status.addEventListener("click", () => {
-      if (status.textContent === "true") {
-        return status.textContent = "false";
+      if (status.textContent === "Yes") {
+        status.classList.remove("yes_sign");
+        // status.style.background = "#e95644";
+        return status.textContent = "No";
       } else {
-        return status.textContent = "true";
+        status.classList.add("yes_sign");
+        // status.style.background = "#68e944";
+        return status.textContent = "Yes";
       }
     });
+
   });
 }
 
@@ -167,8 +194,11 @@ function returnValue() {
     read: selectEl.value
   };
 
-  myLibrary.push(addedBooks);
-  bookBox(myLibrary[myLibrary.length-1].title, myLibrary[myLibrary.length-1].author, myLibrary[myLibrary.length-1].pages, myLibrary[myLibrary.length-1].read);
+  myLibrary.unshift(addedBooks);
+    // for (let i = 0; i < myLibrary.length; i++) {
+    //   console.log(myLibrary[i]);
+    // }
+  bookBox(myLibrary[0].title, myLibrary[0].author, myLibrary[0].pages, myLibrary[0].read);
   deleteButtons();
   changeStatus();
   return setAttributeButton();
